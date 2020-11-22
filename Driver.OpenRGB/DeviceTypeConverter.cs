115fe8c95d71ff56cdd18c76c81eb97a791b5dfc
Jack Pendleton <jacklrpendleton@gmail.com>
2020-11-21 23:12:50 -0600

    Add project files.

---
+++ Driver.OpenRGB/DeviceTypeConverter.cs
@@ -0,0 +1,39 @@
+﻿using OpenRGB.NET.Enums;
+using SimpleLed;
+
+namespace Driver.OpenRGB
+{
+    class DeviceTypeConverter
+    {
+        public static string GetType(DeviceType type)
+        {
+            switch (type)
+            {
+                case DeviceType.Keyboard:
+                    return DeviceTypes.Keyboard;
+                case DeviceType.Mouse:
+                    return DeviceTypes.Mouse;
+                case DeviceType.Mousemat:
+                    return DeviceTypes.MousePad;
+                case DeviceType.Headset:
+                    return DeviceTypes.Headset;
+                case DeviceType.HeadsetStand:
+                    return DeviceTypes.HeadsetStand;
+                case DeviceType.Cooler:
+                    return DeviceTypes.Cooler;
+                case DeviceType.Gpu:
+                    return DeviceTypes.GPU;
+                case DeviceType.Motherboard:
+                    return DeviceTypes.MotherBoard;
+                case DeviceType.Dram:
+                    return DeviceTypes.Memory;
+                case DeviceType.Ledstrip:
+                    return DeviceTypes.LedStrip;
+                case DeviceType.Light:
+                    return DeviceTypes.Bulb;
+                default:
+                    return DeviceTypes.Other;
+            }
+        }
+    }
+}
---
+++ Driver.OpenRGB/OpenRGBDriver.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRGB.NET;
+using OpenRGB.NET.Models;
+using SimpleLed;
+
+namespace Driver.OpenRGB
+{
+    public class OpenRGBDriver : ISimpleLed
+    {
+        public event Events.DeviceChangeEventHandler DeviceAdded;
+        public event Events.DeviceChangeEventHandler DeviceRemoved;
+        public OpenRGBClient client;
+
+        public void Configure(DriverDetails driverDetails)
+        {
+            client = new OpenRGBClient(name: "RGB Sync Studio", autoconnect: true, timeout: 1000);
+
+            var deviceCount = client.GetControllerCount();
+            var devices = client.GetAllControllerData();
+
+            foreach (Device orgbDevice in devices)
+            {
+                ControlDevice slsDevice = new ControlDevice();
+                slsDevice.Driver = this;
+                slsDevice.Name = orgbDevice.Name;
+                slsDevice.DeviceType = DeviceTypeConverter.GetType(orgbDevice.Type);
+                slsDevice.Has2DSupport = false;
+
+                List<ControlDevice.LedUnit> deviceLeds = new List<ControlDevice.LedUnit>();
+
+                int i = 0;
+                foreach (Led orgbLed in orgbDevice.Leds)
+                {
+                    ControlDevice.LedUnit slsLed = new ControlDevice.LedUnit();
+                    slsLed.LEDName = orgbLed.Name;
+                    deviceLeds.Add(slsLed);
+                }
+
+                slsDevice.LEDs = deviceLeds.ToArray();
+
+                DeviceAdded?.Invoke(slsDevice, new Events.DeviceChangeEventArgs(slsDevice));
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetConfig<T>() where T : SLSConfigData
+        {
+            throw new NotImplementedException();
+        }
+
+        public DriverProperties GetProperties()
+        {
+            return new DriverProperties
+            {
+                SupportsPull = false,
+                SupportsPush = true,
+                IsSource = false,
+                SupportsCustomConfig = false,
+                Id = Guid.Parse("2594d8f2-9db5-4efa-a631-7ca2a021fb50"),
+                Author = "Fanman03",
+                Blurb = "Driver that allows OpenRGB devices to be controlled.",
+                CurrentVersion = new ReleaseNumber(1, 0, 0, 1),
+                GitHubLink = "https://github.com/SimpleLed/Driver.OpenRGB",
+                IsPublicRelease = true,
+            };
+        }
+
+        public void InterestedUSBChange(int VID, int PID, bool connected)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name()
+        {
+            return "OpenRGB";
+        }
+
+        public void Pull(ControlDevice controlDevice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(ControlDevice controlDevice)
+        {
+            foreach (ControlDevice.LedUnit slsLED in controlDevice.LEDs)
+            {
+                //TODO figure out how to update LED color
+            }
+        }
+
+        public void PutConfig<T>(T config) where T : SLSConfigData
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
